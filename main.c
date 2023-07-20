1164ec1657803910d3604cb730d9ec077fab6fd7
Sau P <sau@yaaama.xyz>
2023-07-20 09:46:30 +0100

    todo item creation test

--- main.c
+++ main.c
@@ -3,18 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-int main(void) {
-=======
 void test_todo_create(void) {
->>>>>>> fe37a6f (changed totui.c to main.c, can change todo status)
-=======
-void test_todo_create(void) {
-=======
-int main(void) {
->>>>>>> b964d15 (changed totui.c to main.c, can change todo status)
->>>>>>> Stashed changes
 
   todo_item item0 = {"Random todo thing", COMPLETE};
   todo_item item1 = {"Random todo thing2", COMPLETE};
@@ -26,32 +15,9 @@ int main(void) {
   todo_item items[5] = {item0, item1, item2, item3, item4};
 
   for (int i = 0; i < 5; i++) {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    printf("Todo:\t%s\t|\t\tStatus? %s\n",
-           items[i].heading, status_string(items[i].status));
-  }
-
-  return 0;
-}
-=======
-=======
->>>>>>> Stashed changes
     printf("Todo:\t%s\t|\t\tStatus? %s\n", items[i].heading,
            status_string(items[i].status));
   }
 }
 
 int main(void) { return 0; }
-<<<<<<< Updated upstream
->>>>>>> fe37a6f (changed totui.c to main.c, can change todo status)
-=======
-=======
-    printf("Todo:\t%s\t|\t\tStatus? %s\n",
-           items[i].heading, status_string(items[i].status));
-  }
-
-  return 0;
-}
->>>>>>> b964d15 (changed totui.c to main.c, can change todo status)
->>>>>>> Stashed changes
